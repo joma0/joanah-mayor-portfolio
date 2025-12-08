b07ca0364590b47d41a64393cccd23dfaaf448b1
joma0 <joanah.mayor@heig-vd.ch>
2025-12-08 19:59:19 +0100

    installation tailwind et tests

---
+++ src/components/CardProject.vue
@@ -0,0 +1,22 @@
+<script setup>
+import ChipSkill from "./ChipSkill.vue";
+
+const props = defineProps({
+  title: String,
+  skill: String,
+  description: String,
+  features: Array,
+  steps: Array,
+  result: String,
+  technologies: Array,
+});
+</script>
+
+<template>
+  <h1>{{ title }}</h1>
+  <ChipSkill :label="skill"></ChipSkill>
+  <p>{{ description }}</p>
+  <li v-for="feature in features">
+    {{ feature }}
+  </li>
+</template>
